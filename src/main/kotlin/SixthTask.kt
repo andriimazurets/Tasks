3e69c2d5f46e3d88a50146f9ccb3ff5d237a2890
theri <therigasmazurets@gmail.com>
2023-10-31 15:16:34 +0200

    new task

---
+++ src/main/kotlin/SixthTask.kt
@@ -0,0 +1,20 @@
+import java.util.Scanner
+
+// Steps
+// For a given integer input n, create a function that returns a string output of generated steps using the # character.
+// For example, for an input of 3, the output should look like:
+//‘#‘
+//‘##’
+//‘###’
+fun main(){
+    val num = Scanner(System.`in`)
+    println(stepBuilder(num.nextInt()))
+}
+
+fun stepBuilder(num: Int) {
+    var str = "#"
+    repeat(num) {
+        println(str)
+        str += "#"
+    }
+}
